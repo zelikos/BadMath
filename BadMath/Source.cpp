2f1be175e207585d595f773a7dc40fc811e4121e
zelikos <zelikos@pm.me>
2020-11-09 17:22:55 -0500

    Change parameters from int to float; have third input store value in correct variable

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,18 +2,18 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(float i1, float i2, float i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	float n1 = 0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
